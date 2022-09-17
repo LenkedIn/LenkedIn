2b02dea44247e203a779757e203e1217c21efb24
ryanycw <ryan0213.tem04@nctu.edu.tw>
2022-09-17 14:05:20 +0800

    create basic structure of project factory

--- apps/contracts/contracts/ProjectFactory.sol
+++ apps/contracts/contracts/ProjectFactory.sol
@@ -1,44 +1,103 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.9;
 
-contract ProjectFactory {
-    
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import "@semaphore-protocol/contracts/interfaces/ISemaphore.sol";
+import "@semaphore-protocol/contracts/interfaces/IVerifier.sol";
+import "@semaphore-protocol/contracts/base/SemaphoreCore.sol";
+import "@semaphore-protocol/contracts/base/SemaphoreGroups.sol";
+
+error InvalidTreeDepth();
+
+contract ProjectFactory is SemaphoreCore, SemaphoreGroups {
+    uint8 internal constant MAX_DEPTH = 32;
+
+    mapping(uint256 => EnumerableSet.UintSet) internal groupsJoined;
+
     constructor() 
     {
 
     }
 
-    function createProject() 
+    modifier onlySupportedDepth(uint8 depth) 
+    {
+        if(depth > MAX_DEPTH) {
+            revert InvalidTreeDepth();
+        }
+    }
+
+    function createProject(
+        uint256 groupId,
+        uint8 depth,
+        uint256 zeroValue,
+        uint256 identityCommitment
+    ) 
+        external
+        onlySupportedDepth(depth)
+    {
+        _createGroup(groupId, depth, zeroValue);
+        groupsJoined[identityCommitment].add(groupId);
+    }
+
+    function addMember(
+        uint256 groupId, 
+        uint256 identityCommitment
+    ) 
+        external
+    {
+        _addMember(groupId, identityCommitment);
+        groupsJoined[identityCommitment].add(groupId);
+    }
+
+    function removeMember(
+        uint256 groupId,
+        uint256 identityCommitment,
+        uint256[] calldata proofSiblings,
+        uint8[] calldata proofPathIndices
+    ) 
         external
     {
 
     }
 
-    function addMember() 
+    function endProject(uint256 groupId) 
         external
     {
 
     }
 
-    function removeMember() 
+    function getProjectsByIDCommitment(uint256 idCommitment)
         external
     {
 
     }
 
-    function endProject() 
+    function getLeafByIDCommitment(uint256 idCommitment) 
         external
     {
 
     }
 
-    function submitReviews() 
+    function submitReviews(
+        uint256 groupId,
+        string reviewContent,
+        bytes32 signal,
+        uint256 nullifierHash,
+        uint256 externalNullifier,
+        uint256[8] calldata proof
+    ) 
         external
     {
 
     }
 
     function claimReviews() 
+        external
+    {
+
+    }
+
+    function lensConnect()
         external
     {
 
